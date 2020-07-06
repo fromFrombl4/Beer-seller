5e8d961f2bf4ad341243e2026c0ddf3d9abb697f
roman dod <stalkeroma1@mail.ru>
2020-07-07 00:17:02 +0300

    Initial Commit

---
+++ TMS_iOS_05_HA (Beer)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TMS_iOS_05_HA (Beer)
+//
+//  Created by Roman Dod on 7/7/20.
+//  Copyright © 2020 Roman Dod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
